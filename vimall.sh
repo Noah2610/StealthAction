4898f222eaa381f82e045015f08eb7e6bd84ac76
Noah <rosenzweig.noah@gmail.com>
2017-12-22 19:20:19 +0100

    update vimall.sh; rename keybinds.js

--- vimall.sh
+++ vimall.sh
@@ -3,7 +3,7 @@
 if [[ "$1" =~ rb|ruby|game ]]; then
 	vim -c "source ./vimrc" ./StealthAction.rb ./src/rb/*rb ./src/rb/Rooms/* ./src/rb/Instances/*
 elif [[ "$1" =~ js|www|web|frontend|leveleditor|editor ]]; then
-	vim -c "source ./vimrc" ./level-editor/index.html ./level-editor/style.css ./level-editor/js/*
+	vim -c "source ./vimrc" ./level-editor/index.html ./level-editor/keybindings.html ./level-editor/css/* ./level-editor/js/*
 else
 	vim -c "source ./vimrc" ./StealthAction.rb ./src/rb/*rb ./src/rb/Rooms/* ./src/rb/Instances/* ./level-editor/index.html ./level-editor/style.css ./level-editor/js/*
 fi
